b7a28b93ff024822fb8229cfd94a4f4a6f0dd272
Anuja Chavan <chavananuja27@gmail.com>
2025-04-24 21:09:13 +0530

    Add files via upload

---
+++ script-Anuja.js
@@ -0,0 +1,10 @@
+
+const faqListItem = document.querySelectorAll(".inner-container ul li")
+
+
+
+faqListItem.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    item.classList.toggle("show")
+  })
+})
